07b8c9f2f109b26a81854d81891c301e68c7baee
Ryan Hill <ryanhill1128@gmail.com>
2022-01-16 20:52:19 -0600

    initial commit

---
+++ src/main/java/io/rookware/basic_registration_spring_security/BasicRegistrationSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.rookware.basic_registration_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRegistrationSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicRegistrationSpringSecurityApplication.class, args);
+	}
+
+}
